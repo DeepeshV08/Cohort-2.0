c23b63e67200ad61ea8356c396908a19ccd0d6ce
Your Name <deepesh3950lodhi@gmail.com>
2026-04-16 19:40:14 +0530

    Console and promt alert properties

---
+++ JavaScript/Day_27/script.js
@@ -0,0 +1,19 @@
+//console properties
+
+// console.log();
+// console.info();
+// console.warn();
+// console.error();
+// console.table();
+
+
+//prompt / alert
+
+// const name = prompt("Hello ! what is your name")
+// console.log(name)
+
+// alert karta hai 
+// alert("bro sambhal jao")
+
+
+//template string - `` 
