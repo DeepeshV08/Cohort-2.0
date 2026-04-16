0f1f58f59fc8229f63a314c0ee915efa673268a3
Your Name <deepesh3950lodhi@gmail.com>
2026-04-16 20:32:42 +0530

    Datatypes in JS

--- JavaScript/Day_28/script.js
+++ JavaScript/Day_28/script.js
@@ -5,26 +5,39 @@ let b = 20;
 // const is used where value is const no change
 const c = 20;
 
+
+// Comments
+// //single line comment
+// /* Multiline comments */ 
+
+
+
 // data type = data ka type
 
 // float a = 13.3
-
+let d = 13.3
 // number n= 3;
-
+let e = 23;
 // string s = "hello"
-
+let f = "Hello"
 // boolean b = true / false;
-
-// null = jake aapke pass koi value na ho dene ke liye
-
+let g = true;
+// null = jab aapke pass koi value na ho dene ke liye
+let h = null
 // array = ek se jyda value hold karne ke liye
-
-// øbject  = ek se jyda properties hold karne ke liye
-
+let i = [1,2,2,3];
+// object  = ek se jyda properties hold karne ke liye
+let j = {name:"deepesh"}
 // symbol = will leaarn when we will see object in details
 
 // undefined = jab koi variable banaya but usey value nhi di
-
+let l = undefined
 // NaN = koi number ke sath aisa operation jo nhi ho skta
+
 // infinty = infinity
 
+
+//reference - array object functions
+
+//primitive - all other tha refrence data type
+//  jinki value direct copy kar skte hai
