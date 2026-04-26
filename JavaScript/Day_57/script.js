eb5e7a8b191a7889085f1c6a5674e4d306a134be
Your Name <deepesh3950lodhi@gmail.com>
2026-04-26 16:06:39 +0530

    This value in Js

--- JavaScript/Day_57/script.js
+++ JavaScript/Day_57/script.js
@@ -41,3 +41,19 @@ Sketch.prototype.walk = function(){};
 // es6 function inside es5 function inside object - object
 
 
+let obj = {
+    name :"Deepesh"
+}
+function abcd(a,b,c){
+    console.log(this,a,b,c)
+}
+
+abcd.call(obj)
+abcd.apply(obj,[1,2,3])
+
+let newFnc = abcd.bind(obj,1,2,3)
+
+
+// call -> function chalata hai or this ki value set karta hai
+// apply -> wahi karta hai jo call karta hai and arguments mein pehli value this ki and dusri value array hoti hai
+// bind -> wahi karta hai jo call karta hai and aapko new function deta hai
